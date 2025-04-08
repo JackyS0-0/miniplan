6176504506df4a803616c06da6788bce5db83069
JackyS0-0 <949453242@qq.com>
2025-04-08 23:48:19 +0800

    修改代码

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.tencent.wxcloudrun.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
@@ -12,5 +13,11 @@ public class IndexController {
   public ApiResponse get() {
     return ApiResponse.ok("test");
   }
+
+  @GetMapping("/login")
+  public String login(@RequestHeader("X-WX-OPENID") String wx_openid) {
+      return "X-WX-OPENID" + wx_openid;
+  }
+
 
 }
